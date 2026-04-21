36661c94e7814ccd035b6d8de2842a9acbc8de87
jjkkyydijifh <jjkkyydijifh@gmail.com>
2026-04-21 17:51:41 -0400

    Add files via upload

---
+++ WalmartTwitter/app.py
@@ -0,0 +1,127 @@
+from flask import Flask, render_template, jsonify, request, redirect
+import psycopg2
+import math
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+import uuid
+from flask import make_response
+import os
+
+app = Flask(__name__)
+
+limiter = Limiter(get_remote_address, app=app)
+
+
+
+# Database initialization
+
+# Routes
+@app.route("/")
+def index():
+    user_id = request.cookies.get("user_id")
+
+    if not user_id:
+        user_id = str(uuid.uuid4())
+
+    resp = make_response(render_template("index.html"))
+    resp.set_cookie("user_id", user_id)
+
+    return resp
+
+@app.route("/api/posts")
+def get_posts():
+    user_id = request.cookies.get("user_id")
+
+    conn = psycopg2.connect( host=os.environ.get("DB_HOST"), dbname=os.environ.get("DB_NAME"), user=os.environ.get("DB_USER"), password=os.environ.get("DB_PASSWORD"))
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT t.id, t.text, t.likes,
+        EXISTS (
+            SELECT 1 FROM likes l
+            WHERE l.post_id = t.id AND l.user_id = %s
+        ) as liked
+        FROM tweets t
+    """, (user_id,))
+
+    results = cursor.fetchall()
+    conn.close()
+
+    return jsonify(results)
+@app.route("/editor")
+def editor_page():
+ return render_template("editor.html")
+
+@app.route("/api/create_post", methods=["POST"])
+@limiter.limit("5 per minute")
+@limiter.limit("50 per hour")
+def create_post():
+ text = request.form.get("text")
+ likes = request.form.get("likes")
+ date = request.form.get("date")
+ post_id = request.form.get("postID")
+ print(post_id)
+ conn = psycopg2.connect(host=os.environ.get("DB_HOST"), dbname=os.environ.get("DB_NAME"), user=os.environ.get("DB_USER"), password=os.environ.get("DB_PASSWORD"))
+ cursor = conn.cursor()
+ cursor.execute("INSERT INTO tweets (text, likes, date) VALUES (%s, %s, %s) RETURNING id", [text, 0, date])
+ x = cursor.fetchone()
+
+ conn.commit()
+ conn.close()
+ return jsonify(x[0])
+
+
+@app.route("/api/update_post", methods=["POST"])
+def update_post():
+    user_id = request.cookies.get("user_id")
+    post_id = request.form.get("postID")
+    liked = request.form.get("likes") == "true"
+    print(liked)
+    conn = psycopg2.connect( host=os.environ.get("DB_HOST"), dbname=os.environ.get("DB_NAME"), user=os.environ.get("DB_USER"), password=os.environ.get("DB_PASSWORD"))
+    cursor = conn.cursor()
+
+    # check if already liked
+    cursor.execute(
+        "SELECT * FROM likes WHERE user_id = %s AND post_id = %s",
+        (user_id, post_id)
+    )
+    exists = cursor.fetchone()
+    print(exists)
+    if liked:
+        if exists:
+            return "Already liked", 400
+
+        # add like
+        cursor.execute(
+            "INSERT INTO likes (user_id, post_id) VALUES (%s, %s)",
+            (user_id, post_id)
+        )
+
+        cursor.execute(
+            "UPDATE tweets SET likes = likes + 1 WHERE id = %s",
+            (post_id,)
+        )
+
+    else:
+        if  exists:
+            return "Not liked yet", 400
+
+        # remove like
+        cursor.execute(
+            "INSERT INTO likes (user_id, post_id) VALUES (%s, %s)",
+            (user_id, post_id)
+        )
+
+
+        cursor.execute(
+            "UPDATE tweets SET likes = likes - 1 WHERE id = %s",
+            (post_id,)
+        )
+
+    conn.commit()
+    conn.close()
+
+    return "ok"
+
+if __name__ == "__main__":
+    app.run(debug=False)
