03a5ed212f7be2015509df74d36b75f7868f0c63
jjkkyydijifh <jjkkyydijifh@gmail.com>
2026-05-22 21:32:59 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -24,6 +24,24 @@ $('#post-text').on('input', function() {
     $(".char_count").text((($(this).val()).length) + "/180")
 });
 
+$('#searchbar').on('input', function() {
+ if ($('#searchbar').val() != "") {
+    let search = $('#searchbar').val().toLowerCase();
+    $('.post').each(function () {
+        let content = $(this).find('.content').text().toLowerCase();
+        console.log($(this).attr('id'));
+        console.log(content);
+        if (content.includes(search)) {
+            $(this).show();
+        } else {
+            $(this).hide();
+        }
+    });
+} else {
+    $('.post').show();
+}
+   
+});
 
 //indacate if you can or cant post
 $('#post-submit').mouseenter(function() {
@@ -58,6 +76,25 @@ $(document).on('click', '.dislike-button', function() {
   console.log("you disliked post number " + parseInt((($(this).parent()).parent()).parent().attr('id').slice(4)))
   console.log("it has " + parseInt($(this).siblings('.dislike-count').text()) + " dislikes")
   update_tweets("false",parseInt((($(this).parent()).parent()).parent().attr('id').slice(4)),count,$(this))
+});
+
+$(document).on('click', '.report', function() {
+  let count = $(this).find('.report_count');
+  $(this).prop("disabled", true);
+  console.log($(this).parent())
+  console.log("you reported post: " + parseInt((($(this).parent()).parent()).attr('id').slice(4)))
+  let result = confirm("Are you sure you want to report this post.");
+if (result) {
+    console.log("achhcha! hai!")
+    console.log(parseInt((($(this).parent()).parent()).attr('id').slice(4)))
+    console.log(count)
+    console.log($(this))
+
+    report_tweets(parseInt((($(this).parent()).parent()).attr('id').slice(4)),count,$(this))
+} else {
+    console.log("nahin hai :(")
+}
+  
 });
 
 $(document).on('click', '#comment-submit', function() {
@@ -145,14 +182,14 @@ let num_com = 0
       <div id="uhuh">
 
       <div id="likes">
-      <button class="like-button" ${response[index][5] ? "disabled" : ""}><?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 122.88 106.16" style="enable-background:new 0 0 122.88 106.16" xml:space="preserve"><style type="text/css">.st0{fill:#fff;}</style><g><path class="st0" d="M4.02,44.6h27.36c2.21,0,4.02,1.81,4.02,4.03v53.51c0,2.21-1.81,4.03-4.02,4.03H4.02 c-2.21,0-4.02-1.81-4.02-4.03V48.63C0,46.41,1.81,44.6,4.02,44.6L4.02,44.6z M63.06,4.46c2.12-10.75,19.72-0.85,20.88,16.48 c0.35,5.3-0.2,11.47-1.5,18.36l25.15,0c10.46,0.41,19.59,7.9,13.14,20.2c1.47,5.36,1.69,11.65-2.3,14.13 c0.5,8.46-1.84,13.7-6.22,17.84c-0.29,4.23-1.19,7.99-3.23,10.88c-3.38,4.77-6.12,3.63-11.44,3.63H55.07 c-6.73,0-10.4-1.85-14.8-7.37V51.31c12.66-3.42,19.39-20.74,22.79-32.11V4.46L63.06,4.46z"/></g></svg>
+      <button class="like-button" ${response[index][6] ? "disabled" : ""}><?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 122.88 106.16" style="enable-background:new 0 0 122.88 106.16" xml:space="preserve"><style type="text/css">.st0{fill:#fff;}</style><g><path class="st0" d="M4.02,44.6h27.36c2.21,0,4.02,1.81,4.02,4.03v53.51c0,2.21-1.81,4.03-4.02,4.03H4.02 c-2.21,0-4.02-1.81-4.02-4.03V48.63C0,46.41,1.81,44.6,4.02,44.6L4.02,44.6z M63.06,4.46c2.12-10.75,19.72-0.85,20.88,16.48 c0.35,5.3-0.2,11.47-1.5,18.36l25.15,0c10.46,0.41,19.59,7.9,13.14,20.2c1.47,5.36,1.69,11.65-2.3,14.13 c0.5,8.46-1.84,13.7-6.22,17.84c-0.29,4.23-1.19,7.99-3.23,10.88c-3.38,4.77-6.12,3.63-11.44,3.63H55.07 c-6.73,0-10.4-1.85-14.8-7.37V51.31c12.66-3.42,19.39-20.74,22.79-32.11V4.46L63.06,4.46z"/></g></svg>
 </button>
       
       <p class="like-count">${response[index][2]}</p>
       </div> 
 
       <div id="dislikes">
-      <button class="dislike-button" ${response[index][5] ? "disabled" : ""}><?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  viewBox="0 0 122.88 106.16"><style type="text/css">.st0{fill:#fff;}</style><g><path class="st0" d="M4.03,61.56h27.36c2.21,0,4.02-1.81,4.02-4.02V4.03C35.41,1.81,33.6,0,31.39,0H4.03C1.81,0,0,1.81,0,4.03 v53.51C0,59.75,1.81,61.56,4.03,61.56L4.03,61.56z M63.06,101.7c2.12,10.75,19.72,0.85,20.88-16.48c0.35-5.3-0.2-11.47-1.5-18.36 l25.15,0c10.46-0.41,19.59-7.9,13.14-20.2c1.47-5.36,1.69-11.65-2.3-14.13c0.5-8.46-1.84-13.7-6.22-17.84 c-0.29-4.23-1.19-7.99-3.23-10.88c-3.38-4.77-6.12-3.63-11.44-3.63H55.07c-6.73,0-10.4,1.85-14.8,7.37v47.31 c12.66,3.42,19.39,20.74,22.79,32.11V101.7L63.06,101.7L63.06,101.7z"/></g></svg></button>
+      <button class="dislike-button" ${response[index][6] ? "disabled" : ""}><?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  viewBox="0 0 122.88 106.16"><style type="text/css">.st0{fill:#fff;}</style><g><path class="st0" d="M4.03,61.56h27.36c2.21,0,4.02-1.81,4.02-4.02V4.03C35.41,1.81,33.6,0,31.39,0H4.03C1.81,0,0,1.81,0,4.03 v53.51C0,59.75,1.81,61.56,4.03,61.56L4.03,61.56z M63.06,101.7c2.12,10.75,19.72,0.85,20.88-16.48c0.35-5.3-0.2-11.47-1.5-18.36 l25.15,0c10.46-0.41,19.59-7.9,13.14-20.2c1.47-5.36,1.69-11.65-2.3-14.13c0.5-8.46-1.84-13.7-6.22-17.84 c-0.29-4.23-1.19-7.99-3.23-10.88c-3.38-4.77-6.12-3.63-11.44-3.63H55.07c-6.73,0-10.4,1.85-14.8,7.37v47.31 c12.66,3.42,19.39,20.74,22.79,32.11V101.7L63.06,101.7L63.06,101.7z"/></g></svg></button>
       
       <p class="dislike-count">${response[index][3]}</p>
       </div>
@@ -168,7 +205,9 @@ let num_com = 0
 <div class="comment-content">
 
 </div>
-
+<button class="report"><svg xmlns="http://www.w3.org/2000/svg" width="150px" height="150px" viewBox="0 0 24 24" fill="none">
+<path d="M4.5 21V16M4.5 16V6.5C5.5 5.5 7 5 8.5 5C11.5 5 13.5 7.5 17.5 5.5V15.5C13.5 17.5 11.5 14.5 8.5 14.5C7.5 14.5 5.5 15 4.5 16Z" stroke="#ffffff">
+</svg><p class="report_count">${response[index][5]}</p></button>
       </div>
       </div>
   `;
@@ -233,6 +272,10 @@ let postHTML = `
 <div class="comment-content">
 
 
+<button class="report"><svg xmlns="http://www.w3.org/2000/svg" width="150px" height="150px" viewBox="0 0 24 24" fill="none">
+<path d="M4.5 21V16M4.5 16V6.5C5.5 5.5 7 5 8.5 5C11.5 5 13.5 7.5 17.5 5.5V15.5C13.5 17.5 11.5 14.5 8.5 14.5C7.5 14.5 5.5 15 4.5 16Z" stroke="#ffffff">
+</svg><p class="report_count">0</p></button>
+
       </div>
   `;
 
@@ -271,6 +314,29 @@ let parent = button.parent();
         }
  });
 }
+
+function report_tweets(POEID, count, button){
+console.log("id is " + POEID)
+count.text(parseInt(count.text()) + 1);
+  
+$.ajax({
+ url: "/api/report_post",
+ type: 'POST',
+ data: {
+  postID: POEID,
+ },
+ success: function (response) {
+  console.log(response)
+ },
+ error: function(err) {
+        console.log(err)
+        count.text(parseInt(count.text()) - 1);
+        button.prop("disabled", false);
+    
+        }
+ });
+}
+
 
 })
 
